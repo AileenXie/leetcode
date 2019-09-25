bb3bdba4c2f74bc042996240d4e0db52b0344ed2
aileen <yiyingxie@126.com>
2019-09-26 00:06:17 +0800

    update

---
+++ 020_valid_parentheses.py
@@ -0,0 +1,49 @@
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+#
+# Example 1:
+#
+# Input: "()"
+# Output: true
+# Example 2:
+#
+# Input: "()[]{}"
+# Output: true
+# Example 3:
+#
+# Input: "(]"
+# Output: false
+# Example 4:
+#
+# Input: "([)]"
+# Output: false
+# Example 5:
+#
+# Input: "{[]}"
+# Output: true
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_map = {"(": ")", "[": "]", "{": "}"}
+        push_list = ["(", "[", "{"]
+        stack = []
+        for item in s:
+            if item in push_list:
+                stack.append(item)
+            elif stack and item == bracket_map[stack[-1]]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
+
+
+if __name__ == '__main__':
+    # result = Solution().isValid("([)]")
+    result = Solution().isValid("{[]}")
+    print(result)
